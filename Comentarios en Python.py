7ad4f4219ce26e50168618292488238cd2511b92
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-24 17:28:30 -0500

    Comentarios en Python

---
+++ Comentarios en Python.py
@@ -0,0 +1,9 @@
+#Practicando comentarios con la almohadilla. Esto es un comentario.
+print ("Hola") 
+
+"Esto es otro comentario, pero esta vez estoy usando comillas"
+#Las comillas no son comentarios, sino strings, pero al no asignarles una variable, se interpretan como valor nulo
+
+"""Esto es un comentario con comillas triples.
+Es perfecto para crear comentarios multilínea
+"""
