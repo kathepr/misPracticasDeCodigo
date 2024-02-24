8e919aed9732453a88f9be31b476236a8c40db40
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-24 17:06:52 -0500

    DIVISIÓN ENTERA

---
+++ Operadores aritméticos - División Entera.py
@@ -0,0 +1,10 @@
+print ("DIVISIÓN ENTERA:") #El resultado será un valor entero, no obtendras decimales.
+
+num1 = 4
+num2 = 2
+
+resultado = num1 // num2
+
+resultado = str(resultado)
+
+print ("El resultado de la división entera es: " + resultado)
