b6a3dbaa69dcd33749abcfe94cbf5a14409047a9
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-24 18:36:44 -0500

    Ejercicio Entrada de Datos

---
+++ Entrada de datos 2.py
@@ -0,0 +1,8 @@
+nombre = input ("¡Hola! ¿Como te llamas? ")
+print ("¿Que hay " + nombre + "? Vamos a realizar una suma.")
+num1 = int (input("Ingresa el primer número entero: "))
+num2 = int (input("Ingresa el segundo número entero: "))
+
+resultado = num1+num2
+
+print (nombre + " el resultado de la suma es: ", resultado)
