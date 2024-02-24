6e9d868578cdabff55d3a3678fab52239a8f8cdb
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-24 15:51:08 -0500

    Exponente - Potenciación

---
+++ Operadores aritméticos - Potenciación o  Exponente.py
@@ -0,0 +1,10 @@
+print ("EXPONENTE / POTENCIACIÓN:")
+
+num1 = 2
+exponente = 5
+
+resultado = num1 ** exponente
+
+resultado = str(resultado)
+
+print ("El resultado de la potenciación es: " + resultado)
