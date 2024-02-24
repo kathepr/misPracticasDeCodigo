8064677cd9c12d174c1a2a3b47fe20bd19fc606e
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-24 10:16:17 -0500

    Operacion cadena - Busqueda

---
+++ Operacion Cadena - Búsqueda.py
@@ -0,0 +1,5 @@
+print ("BÚSQUEDA:")
+
+mensaje = "Hola Kathe"
+buscar_subcadena = mensaje.find("Kathe") # Usamos .find para encontrar dento del mensaje, la palabra que buscamos
+print (buscar_subcadena) #Como resultado nos arroja un número. Este nos dice ñla posición de la palabra que colocamos. 
