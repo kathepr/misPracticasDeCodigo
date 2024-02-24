4a6c15dfe9300eabb085dea0b64139a0d829528c
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-24 10:46:53 -0500

    se añadió la palabra extracción

--- Operacion Cadena - Extracción.py
+++ Operacion Cadena - Extracción.py
@@ -3,4 +3,5 @@ mensaje=("Hola Kathe, ¿Como estás?")
 extraer_subcadena= mensaje[1:8] #OJO: El primer número es la posición del primer carcater que queremos extraer, el último número es la última posición.
 # Sin embargo, el programa va a extraer un caracter amtes de la posición final
 
+print ("EXTRACCIÓN:")
 print (extraer_subcadena)
