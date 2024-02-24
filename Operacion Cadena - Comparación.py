4197bac906005a851d09d0503414193071d9d869
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-24 10:56:36 -0500

    Operacion Cadena - Comparacion

---
+++ Operacion Cadena - Comparación.py
@@ -0,0 +1,9 @@
+mensaje1 = "Hola"
+mensaje2 = "hola"
+
+print (mensaje1==mensaje2) #FALSE (porque son diferentes)
+
+mensaje1= "Kathe"
+mensaje2= "Kathe"
+
+print (mensaje1==mensaje2) #TRUE (porque son iguales)
