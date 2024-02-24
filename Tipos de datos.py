5316f105162ab8269d2c156630b97687989c0cf5
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-24 18:07:34 -0500

    Tipos de datos

---
+++ Tipos de datos.py
@@ -0,0 +1,26 @@
+#Tipo de dato: ENTERO
+
+numero = 15
+print (numero, type(numero)) # type(nombre de la variable), nos muestra el tipo de dato al que pertenece
+#Cuando usamos comas, esto nos ayuda a separar datos diferentes y colocar varios elementos dentro de print
+
+
+#Tipo de dato: FLOTANTE
+numFlotante = 15.5
+print (numFlotante, type(numFlotante))
+
+#Tipo de dato: NÚMERO COMPLEJO
+numComplejo = 5 + 6j
+print (numComplejo, type(numComplejo))
+
+#Tipo de dato: CADENA / STRING
+nombre = "Kathe"
+print (nombre, type(nombre))
+
+#Tipo de dato: BOOLEANO
+verdadero_falso = 3==3 # Cuando se colocan dos iguales ==, se está comparando si esos dos datos son iguales
+print (verdadero_falso, type (verdadero_falso))
+
+
+
+
