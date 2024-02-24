e255b14199252294e08bf03ecee0cd64324436a5
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-24 10:00:01 -0500

    Operación cadena - Concatenación

---
+++ Operacion Cadena - Concatenación.py
@@ -0,0 +1,15 @@
+print ("Concatenación:")
+
+mensaje = "Hola"
+espacio = " "
+nombre = "Kathe"
+
+print (mensaje + espacio + nombre) #Se unen dos cadenas o más, para formar una cadena de mayor tamaño. Se usa el operado: +
+
+
+num1= 2
+num2= 4
+resultado = num1 + num2 #El programa detecta cuando queremos hacer una suma y cuando concatenaciones
+resultado = str (resultado) #Aquí estoy convirtiendo una variable de valor númerico a un string. Porque no se puede concatenar valores string con valores enteros. Ambos necesitan tener la misma naturaleza.
+
+print ("El resultado de la suma es: " + resultado) #En esta parte se hizo concatenacion
