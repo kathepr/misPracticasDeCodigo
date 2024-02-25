0428c8aa77559ccc0eadec5c1d13f93111ed1c6d
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-25 18:08:13 -0500

    Ciclo / Bucle While

---
+++ Ciclo - Bucle WHILE.py
@@ -0,0 +1,24 @@
+#Ejemplo 1
+
+x = 1
+
+while x < 3:
+    print (x)
+    x+=1
+
+print ("Fin.")
+
+#Ejemplo 2 : Mandar a imprimir un nombre 10 veces
+
+nombre = input ("¿Como te llamas? ")
+print ("Vamos a mostrar tu nombre 10 veces")
+
+y=1
+while y<=10:
+    print (y,nombre,sep="." )
+    y +=1
+
+print ("Fin.")    
+
+
+
