e31de3e43aa2527d900393e8f11197890a903cbd
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-25 17:31:48 -0500

    parametro end

---
+++ Parametro end.py
@@ -0,0 +1,7 @@
+print ("Esto es un") #Cada vez que print termina su ejecución se genera un salto de línea
+print ("ejemplo")    # y continua con la siguiente instrucción. 
+                     
+print (" ")                     #El parametro end, nos va a ayudar a evitar ese salto de línea.
+
+print ("Esto es un", end=" ")
+print ("ejemplo con el parametro end")
