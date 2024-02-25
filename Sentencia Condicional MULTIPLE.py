846e4aaf8a3bdc528bbe21d119338e752a695f61
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-24 21:19:45 -0500

    Sentencia Condicional MULTIPLE

---
+++ Sentencia Condicional MULTIPLE.py
@@ -0,0 +1,31 @@
+print ("========================================")
+print ("    Convertidor de letras a números")
+print ("========================================")
+
+num=int(input("¿Cuál es el número que deseas convertir a letra? "))
+
+if num == 1:
+    print ("El número es 'UNO'")
+
+elif num == 2:
+    print ("El número es 'DOS'")
+
+elif num == 3:
+    print ("El número es 'TRES'")
+
+elif num == 4:
+    print ("El número es 'CUATRO'")
+
+elif num == 5:
+    print ("El número es 'CINCO'")   
+
+else:
+    print ("Este programa solo puede convertir hasta el número 5")
+
+print ("Fin.")        
+
+
+
+
+
+
