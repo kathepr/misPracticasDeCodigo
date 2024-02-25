191cf93e93f6acd88d4e456605f67c91bbb496f3
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-25 15:02:00 -0500

    Operadores de asignación: suma y resta

---
+++ Operadores de Asignación.py
@@ -0,0 +1,16 @@
+nombre = "Hola "
+nombre += input ("¿Como te llamas? ")
+
+print (nombre, ", esto es el incremento y decremento de una variable. \n")
+print ("Incremento o Aumento:")
+x=1
+print ("El valor inicial de x es: ", x)
+x+=4
+print ("El valor final de x es: ", x, "\n")
+
+
+print ("Decremento o Disminución: ")
+
+print ("El valor inicial de x es: ", x)
+x-=4
+print ("El valor final de x es: ", x, "\n")
