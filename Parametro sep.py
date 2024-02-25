29989459bb0517467573a31bb013218c4239a4fa
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-25 17:42:16 -0500

    Parametro sep

---
+++ Parametro sep.py
@@ -0,0 +1,9 @@
+print ("1","2","3","4","5") #En este caso, los valores no tendrán ningun espacio
+                            #Python se encarga de agregar espacio cuando colocamos una serie de caracteres
+                            #Pero ¿Como quitamos el espacio o agregamos algo entre esos espacios?
+
+print ("1","2","3","4","5", sep="") #Si no coloco un espacio entre las comillas del sep
+                                    #Se elimina el espacio que automaticamente pone Python entre una serie de caracteres
+
+
+print ("1","2","3","4","5", sep=",") #Puedo añadir caracteres entre los espacios, como una coma (,)
