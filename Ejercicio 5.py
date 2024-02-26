d9ece660227f30c9fb9a56d837cecc8572de73ca
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-25 19:09:40 -0500

    Sucesión de Fibonacci

---
+++ Ejercicio 5.py
@@ -0,0 +1,19 @@
+#Sucesión de Fibonacci
+
+num1 = 0
+num2 = 1
+i = 1
+while i<=9:
+
+    print (num1, num2, end = " ")  
+    num1 +=num2
+    num2 += num1
+    i+=1
+
+print ("\n")
+print ("Fin.")    
+
+
+
+
+    
