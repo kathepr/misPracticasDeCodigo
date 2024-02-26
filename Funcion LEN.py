070edc8ad6c138d4c7e7c11e3464f8d078abe5f1
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-26 17:11:39 -0500

    Función len ( )

---
+++ Funcion LEN.py
@@ -0,0 +1,8 @@
+#Funcion len ()
+
+#Opción 1:
+print ("'Hola' tiene ", len("Hola"), " caracteres.")
+
+#Opción 2
+longitud = len ("La Gekkipedia")
+print ("'La Gekkipedia' tiene ", longitud, " caracteres.")
