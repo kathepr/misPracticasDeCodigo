94f3d379d1be3d98f12b5476cd67091ffb12016f
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-26 18:25:23 -0500

    Concatenación f-Strings

---
+++ Concatenación con f-Strings.py
@@ -0,0 +1,29 @@
+#Ejemplo 1
+
+nombre = "Susana"
+edad = 22
+
+print(f"¡Hola {nombre}! Tienes {edad} años.")
+
+#Ejemplo 2
+
+print(f"{5+1}") # Con las llaves indicamos que hay una expresión o variable
+
+
+
+#Ejemplo 3
+
+nombre = "Felipe"
+estatura = 1.70
+edad = 22
+
+print(f"Hola {nombre}! Mides {estatura} y tienes {edad} años.")
+
+
+#Ejemplo 4
+
+nombre=input("¿Cual es tu nombre? ")
+num1=int(input("Introduce un número: "))
+num2=int(input("Introduce un segundo número: "))
+
+print(f"¡Hola {nombre}! La suma de los dos números es: {num1 + num2} ")
