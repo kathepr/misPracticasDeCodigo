bf35a07eb0952c02f11c6afb522f457ae601d4f3
kathepr <160674528+kathepr@users.noreply.github.com>
2024-06-01 13:44:44 -0500

    print modifiers

---
+++ Python/Print - Distintos tipos de Print.py
@@ -0,0 +1,6 @@
+# Distintos tipos de PRINT
+
+print(" ")
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
