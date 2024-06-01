012c90c9513fb05e6343df2b0bd347d1d687048c
kathepr <160674528+kathepr@users.noreply.github.com>
2024-06-01 16:42:37 -0500

    Formas de hacer un input

---
+++ Python/Input.py
@@ -0,0 +1,6 @@
+#Formas de hacer un input
+
+print("\n")
+print("Hello " + input("What's your name? ") + "!\n" )
+
+print(len(input("Write a word: "))) #En una linea tambien podemos simplificar todo
