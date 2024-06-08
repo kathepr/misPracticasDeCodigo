03237d313f985f06c55d999ce889139365554ed5
kathepr <160674528+kathepr@users.noreply.github.com>
2024-06-08 12:11:26 -0500

    feat :tada: Recordando loops

---
+++ Python/Practica loops.py
@@ -0,0 +1,34 @@
+#LOOPS
+
+# Primer tipo de LOOP
+# for item in list_of_items:
+#     Do something to each item
+
+fruits = ["apple", "peach", "pear"]
+
+for item in fruits: #Ayuda para pasar por todos los elementos de una lista
+    print(item)
+    print(item + " pie")
+
+
+
+#Segundo Tipo de LOOP
+# for number in range(1,10):
+#     print(number)
+
+print("\nFor con Range\n")
+
+for number in range(1,10):
+    print(number) #No imprime el ultimo digito. Si quiero que el 10 se imprima, debo poner el rango en 11
+
+print("\nFor con Range y número extra\n")
+
+for number2 in range(2, 11, 2): #Para que se incremente de dos en dos, debo poner un tercer número, en este caso, dos
+    print(number2)
+
+print("\nSuma de numeros del 1 al 100\n")
+total = 0
+for number100 in range(1, 101):
+    total += number100
+
+print(total)
