488b43d58c9fb480fb15b7a64e776375f2860747
kathepr <160674528+kathepr@users.noreply.github.com>
2024-06-26 06:44:35 -0500

    feat ✨ Distintas formas de definir variables (todas son correctas

--- JavaScript/otros ejercicios/js/main.js
+++ JavaScript/otros ejercicios/js/main.js
@@ -1,1 +1,18 @@
 alert("¡Soy JavaScript desde un script externo!")
+
+
+//Distintas formas de definir variables, todas son correctas. 
+
+let user = "Kathe";
+let age = 30;
+let message = "Hola a Todos"
+
+
+let user2 = "Kathe",
+    age2 = 30,
+    message2 = "Hola a Todos"
+
+let user3 = "Kathe"
+    , age3 = 30
+    , message3 = "Hola a Todos"
+
