5f64c68f63b032e0d8eceedf4a9fe1263005abc2
kathepr <160674528+kathepr@users.noreply.github.com>
2024-07-08 19:00:12 -0500

    feat ✨ Creación de constructor

--- JavaScript/Programacion Orientada a Objetos/main.js
+++ JavaScript/Programacion Orientada a Objetos/main.js
@@ -55,4 +55,50 @@ const car = {
 
 
 
+// *******************************************************
+
+
+
+//Constructor: Es como un generador de objetos, nos evita estar repitiendo miles de datos (no tenemos que crear objetos literales) y lo que hace es crear un tipo de "plantilla". Son funciones.
+
+
+const usuario1 = {
+    name: "Katheryn",
+    lastname: "PR",
+    age: "30", 
+    fullName(){
+        return `${this.name} ${this.lastname}`;
+    } // Methonds, también podemos poner la funcion adentro del objeto.
+}
+
+
+//Este es el CONSTRUCTOR de objetos
+function Persona(){
+    //Se coloco P mayuscula, para hacer referencia a un constructor
+    this.name = "",
+    this.lastname = "",
+    this.age = 0
+    this.fullName = function(){
+        return `${this.name} ${this.lastname}`;
+    }
+} 
+
+//palabra clave para constructures: new
+const usuario2 = new Persona()
+usuario2.name = "Matilda"
+usuario2.lastname = "Wormwood"
+usuario2.age = 6
+console.log(usuario2.fullName())
+
+
+//palabra clave para constructures: new
+const usuario3 = new Persona()
+usuario3.name = "Hermione"
+usuario3.lastname = "Granger"
+usuario3.age = 44
+console.log(usuario3.fullName())
+
+
+
+
 
