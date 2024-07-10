f85d03340915ff73a25b9a524646e4a310f77bd6
kathepr <160674528+kathepr@users.noreply.github.com>
2024-07-09 19:48:35 -0500

    feat ✨ MySQL select

---
+++ SQL/select.sql
@@ -0,0 +1,10 @@
+--Sentencia de Selección
+-- Me muestra TODA la tabla
+SELECT  * FROM users;
+
+
+--Se muestran solo los nombres
+SELECT name FROM users;
+
+--Se muestra ID y nombre
+SELECT user_id, name FROM users;
